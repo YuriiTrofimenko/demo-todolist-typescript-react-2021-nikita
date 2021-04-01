e9450321aeb687d465149e38cb5e3264cc245ea8
YuriiTrofimenko <tyaa@ukr.net>
2021-04-01 12:45:10 +0300

    Custom App Component View, State and useEffect hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 /* Functional Root Component */
 function App () {
+  // State
+  const [date, setDate] = useState(<div>{new Date().toLocaleTimeString('ru')}</div>)
+  // Lifecycle Handlers
+  useEffect(() => {
+    console.log('useEffect')
+    setInterval(() => {
+      setDate(<div>{new Date().toLocaleTimeString('ru')}</div>)
+      console.log(new Date().toLocaleTimeString('ru'))
+    }, 1000)
+  }, [])
+  // Temporary constants, variables and logic
+  // ...
   // View
-  return <h1>Hello React!</h1>
+  return (
+    <>
+      <h1>Hello React!</h1>
+      {date}
+    </>
+  )
 }
 
 export default App
