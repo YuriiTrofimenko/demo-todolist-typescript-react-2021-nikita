259711a7566ec9d3db6b931133aa08ab41345d21
YuriiTrofimenko <tyaa@ukr.net>
2021-04-13 10:50:48 +0300

    React-MobX integration: commonStore to App

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,21 +1,41 @@
+import { inject, observer } from 'mobx-react'
 import React/* , { useState, useEffect } */ from 'react'
 import Footer from './common/Footer'
 import Header from './common/Header'
+import { CommonStore } from '../stores/CommonStore'
 
+// интерфейс той части внешних параметров,
+// значения которых передаются в компонент App неявно,
+// внедрением зависимости, то есть, например,
+// нигде в коде представлений других компонент не встречается запись вида:
+// <App commonStore={commonStore} />
+interface IInjectedProps extends IProps {
+  commonStore: CommonStore
+}
 interface IProps {}
 interface IState {
   date: string
 }
 
 /* Class Root Component */
+@inject('commonStore')
+@observer
 class App extends React.Component<IProps, IState> {
   // State
   // const [date, setDate] = useState(<div>{new Date().toLocaleTimeString('ru')}</div>)
   constructor (props: IProps) {
+    // вызов конструктора класса родителя React.Component
     super(props)
+    // инициализация состояния компонента
     this.state = {
       date: new Date().toLocaleTimeString('ru')
     }
+  }
+  // свойство компонента, доступное только для чтения
+  get injected () {
+    // возвращает ссылку на словарь внешних параметров props,
+    // типизированную интерфейсом IInjectedProps
+    return this.props as IInjectedProps
   }
   // Lifecycle Handlers
   /* useEffect(() => {
@@ -26,22 +46,29 @@ class App extends React.Component<IProps, IState> {
     }, 1000)
   }, []) */
   componentDidMount () {
-    console.log('componentDidMount')
+    // console.log('componentDidMount')
     setInterval(() => {
       this.setState({date: new Date().toLocaleTimeString('ru')})
       console.log(new Date().toLocaleTimeString('ru'))
     }, 1000)
+    // debug code: try common store loading property
+    this.injected.commonStore.setLoading(true)
+    setTimeout(() => {
+      this.injected.commonStore.setLoading(false)
+    }, 5000)
   }
   // Temporary constants, variables and logic
   // ...
   // View
   render () {
+    const loadingBlock = this.injected.commonStore.loading ? <div>Loading ...</div> : ''
     return (
       <>
         {/*<h1>Hello React!</h1>*/}
         <Header headerText='Hello React!'>
           Subtitle
         </Header>
+        {loadingBlock}
         <div>
           {this.state.date}
         </div>
--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,24 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './components/App'
+// импорт по умолчанию экземпляра хранилища CommonStore
+import commonStore from './stores/CommonStore'
+import { Provider } from 'mobx-react'
+
+// ссылки на все объекты хранилищ
+const stores = {
+  commonStore
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* завертывание экземпляра App в невидимый компонент Provider,
+    который предоставляет возможность компоненту App и всем вложенным в него компонентам
+    запросить внедрение зависимости любого из модулей хранилища,
+    перечисленных в объекте stores */}
+    <Provider {...stores}>
+      <App/>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 )
